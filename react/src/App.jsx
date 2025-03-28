c8dff4df08933e03a8845d1ed3750d2cb6612d15
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-03-28 13:28:05 +0530

    test

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Bhumireddy Sahithi</h1>
+      <h2>bhumireddy.sahithi.s89@kalvium.community</h2>
+    </>
+  )
+}
+
+export default App
